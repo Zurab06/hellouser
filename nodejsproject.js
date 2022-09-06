248c56352c963ae56527541cc266c9216d832845
gaunt0066 <gaunt0066@gmail.com>
2022-09-06 09:48:27 +0300

    done

---
+++ nodejsproject.js
@@ -0,0 +1,5 @@
+import os, { homedir, hostname,} from 'os';
+console.log(homedir())
+console.log(hostname())
+console.log(os.version())
+console.log(os.type())
---
+++ welcome.js
@@ -0,0 +1,3 @@
+import os from 'os';
+export function osversion(){
+return `Привет,юзер. Вижу ты зашел с ${os.type()}`}
